56c082a4e3760830796ea29f0556a99404eea67d
shisus <laimejesu@gmail.com>
2023-06-21 11:22:36 +0200

    add tests for new from hypergraph to concept feature

--- test/testHypergraph.py
+++ test/testHypergraph.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.hypergraph import _cartesian, _dimensions, _tuplesToList, buildHypergraphComplement
+from src.hypergraph import _cartesian, _dimensions, _tuplesToList, buildHypergraphEdges, fromHypergraphToConcepts
 
 
 class TestHypergraph(unittest.TestCase):
@@ -25,15 +25,28 @@ class TestHypergraph(unittest.TestCase):
 
     def test_buildHypergraphEmpty(self):
         context = [[], 0]
-        hg = buildHypergraphComplement(context)
+        hg = buildHypergraphEdges(context)
         self.assertEqual(hg, [])
 
     def test_buildHypergraphWithContext(self):
         context = [[[0, 1], [1, 2], [2, 3]], 3, 3]
-        hg = buildHypergraphComplement(context)
+        hg = buildHypergraphEdges(context)
         expected = [[0, 3], [0, 5], [1, 3], [1, 4], [2, 3], [2, 4], [2, 5]]
         self.assertEqual(hg, expected)
 
+    def test_fromHypergraphToConceptsShouldNotBeEmpty(self):
+        hypergraph = """"""
+        self.assertRaises(ValueError, fromHypergraphToConcepts, hypergraph)
+
+    def test_fromHypergraphToConceptsShouldContainMoreThanOneLine(self):
+        hypergraph = """1 2 3"""
+        self.assertRaises(ValueError, fromHypergraphToConcepts, hypergraph)
+
+    def test_fromHypergraphToConceptsCreatesConcept(self):
+        hypergraph = """3 5 4\n3 5 1 2\n0 3 4 2\n0 1 2\n4\n\n0\n0\n0\n2\n2"""
+        concept = fromHypergraphToConcepts(hypergraph)
+        expected = [['3', '5', '4'], ['3', '5', '1', '2'], ['0', '3', '4', '2'], ['0', '1', '2']]
+        self.assertEqual(concept, expected)
 
 if __name__ == '__main__':
     unittest.main()
