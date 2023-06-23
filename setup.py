17a2cbfb8a545c7cf1039330fd598c865951b208
shisus <laimejesu@gmail.com>
2023-06-23 16:35:23 +0200

    fixes setup build command

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    packages=find_packages(exclude=['test']) + ['src/pca/scripts'],
+    # @TODO to evade warning when running "python -m build", the ["pca.scripts"] package was added
+    packages = find_packages(exclude=["test"]) + ["pca.scripts"],
     include_package_data=True,
 )
